ca389cd01a974e7f4f72bd77c28b02b649d93ee4
Jose Miguel Martín Hidalgo <josemiguel983469@gmail.com>
2016-10-26 09:33:19 +0200

    añadidos más ejercicios resueltos del capítulo 5

--- Ejercicios Capítulo 5/Ejercicio05.java
+++ Ejercicios Capítulo 5/Ejercicio05.java
@@ -1,16 +1,21 @@
-/* Ejercicio 4 - Capítulo 5
+/* Ejercicio 5 - Capítulo 5
  * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás
- * utilizando un bucle for.
+ * utilizando un bucle while.
  * Hecho por Jose Miguel Martín Hidalgo
  */
 
-public class Ejercicio04 {
+public class Ejercicio05 {
   public static void main (String[] args) {
     
-    System.out.println("Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando un bucle for");
+    System.out.println("Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando un bucle while");
     System.out.println("----------------------------------------------------------------------------------------------");
-    for (int numero = 320; numero >= 160; numero = numero - 20) {
+    
+    int numero = 320;
+    
+    while (numero >= 160) {
+      
       System.out.println(numero);
+      numero = numero - 20;
     }
   }
 }
